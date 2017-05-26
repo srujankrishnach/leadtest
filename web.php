2c69c896563a74d21359de66ec851ff5d8a05f62
srujankrishnach <ch.srujan@yahoo.com>
2017-05-26 15:56:34 +0530

    updated

--- web.php
+++ web.php
@@ -3,4 +3,8 @@ $challenge = $_REQUEST['hub_challenge'];
 $verify_token = $_REQUEST['hub_verify_token'];
 if($verify_token==='abc123')
 	echo $challenge;
+
+$inputJSON = file_get_contents('php://input');
+$input = json_decode($inputJSON, true);
+error_log(print_r($input, true));
 ?> 
