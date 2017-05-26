45ffccd964191d584662c94436fa32c7865ddf1a
srujankrishnach <ch.srujan@yahoo.com>
2017-05-26 17:15:52 +0530

    'updated'

--- web.php
+++ web.php
@@ -7,4 +7,9 @@ if($verify_token==='abc123')
 $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 error_log(print_r($input, true));
+
+$var1 = 'some_value';
+$var2 = 'some_other_value';
+header('Location: http://192.168.64.1:9090/fbtest/getData.jsp?var1='.$var1.'&var2='.$var2);
+exit;
 ?> 
