50730b6bce8a60b9fd3887948f934aba65a099b4
srujankrishnach <ch.srujan@yahoo.com>
2017-05-26 15:23:04 +0530

    updated

--- web.php
+++ web.php
@@ -1,11 +1,7 @@
-<html>
-<body>
-
-<h1>My first PHP page</h1>
-
 <?php
 echo "Hello World!";
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+if($verify_token==='abc123')
+	echo $challenge;
 ?> 
-
-</body>
-</html>
