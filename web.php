500a7eb9a16900119c1fdedca9ac08e75969a80e
srujankrishnach <ch.srujan@yahoo.com>
2017-05-26 17:22:44 +0530

    'updated'

--- web.php
+++ web.php
@@ -8,8 +8,26 @@ $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 error_log(print_r($input, true));
 
-$var1 = 'some_value';
-$var2 = 'some_other_value';
-header('Location: http://192.168.64.1:9090/fbtest/getData.jsp?var1='.$var1.'&var2='.$var2);
-exit;
+ ob_start();
+
+  $var1 = 'some_value';
+  $var2 = 'some_other_value';
+ 
+  ob_end_clean();
+
+  /* 
+     Make sure you url-encode your variables so they don't break!
+  */
+  $location  = 'http://192.168.64.1:9090/fbtest/getData.jsp?';
+  $location .= 'var1='.url_encode($var1);
+  $location .= '&amp;';
+  $location .= 'var2='.url_encode($var2);
+
+  /*
+     Redirect to $location, 
+     Using a 301 redirect (not 302!), 
+     With TRUE to replace default HTTP code with 301 code and redirect.
+  */
+  header('Location: '.$location, 301, TRUE);
+  exit();
 ?> 
