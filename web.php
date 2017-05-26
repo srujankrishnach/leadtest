71e70bf4887c044fb258239b36694e7de57b5f94
srujankrishnach <ch.srujan@yahoo.com>
2017-05-26 17:57:40 +0530

    dd

--- web.php
+++ web.php
@@ -8,9 +8,9 @@ $inputJSON = file_get_contents('php://input');
 $input = json_decode($inputJSON, true);
 error_log(print_r($input, true));
 
-$var1 = 'some_value';
-$var2 = $input;
+//$var1 = 'some_value';
+//$var2 = $input;
 
-header('Location: http://192.168.64.1:9090/fbtest/getData.jsp?var1='.$var1.'&var2='.$var2);
-exit;
+//header('Location: http://192.168.64.1:9090/fbtest/getData.jsp?var1='.$var1.'&var2='.$var2);
+//exit;
 ?> 
